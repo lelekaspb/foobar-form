4c48a71816134d6a68894889da284970ccc8e80d
lelekaspb <bov-1986@yandex.ru>
2021-12-26 20:36:51 +0100

    changed from useState to useReducer

--- src/components/Creditcard.js
+++ src/components/Creditcard.js
@@ -3,7 +3,7 @@ import Backlink from "./Backlink";
 import SuccessMessage from "./SuccessMessage";
 import ErrorMessage from "./ErrorMessage";
 import MaskedInput from "react-text-mask";
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect, useReducer } from "react";
 import { postOrder } from "./../utilities/post.js";
 import { useNavigate } from "react-router-dom";
 
@@ -15,15 +15,42 @@ function Creditcard() {
     navigate("/confirmation");
   };
 
-  // states for storing input values and keeping track of errors
-  const [number, setNumber] = useState("");
-  const [numberErr, setNumberErr] = useState(false);
-  const [name, setName] = useState("");
-  const [nameErr, setNameErr] = useState(false);
-  const [expiry, setExpiry] = useState("");
-  const [expiryErr, setExpiryErr] = useState(false);
-  const [cvc, setCvc] = useState("");
-  const [cvcErr, setCvcErr] = useState(false);
+  const initialState = {
+    number: "",
+    numberErr: false,
+    name: "",
+    nameErr: false,
+    expiry: "",
+    expiryErr: false,
+    cvc: "",
+    cvcErr: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "number":
+        return { ...state, number: action.data };
+      case "numberErr":
+        return { ...state, numberErr: action.data };
+      case "name":
+        return { ...state, name: action.data };
+      case "nameErr":
+        return { ...state, nameErr: action.data };
+      case "expiry":
+        return { ...state, expiry: action.data };
+      case "expiryErr":
+        return { ...state, expiryErr: action.data };
+      case "cvc":
+        return { ...state, cvc: action.data };
+      case "cvcErr":
+        return { ...state, cvcErr: action.data };
+
+      default:
+        throw new Error();
+    }
+  }
 
   // refs for accessing input elements
   const cardNumberRef = useRef("");
@@ -34,77 +61,65 @@ function Creditcard() {
 
   // useEffect functions are used for changing focus from one input to another
   useEffect(() => {
-    if (number.length === 19) {
+    if (state.number.length === 19) {
       cardNameRef.current.focus();
     }
-  }, [number]);
+  }, [state.number]);
 
   useEffect(() => {
-    if (expiry.length === 5) {
+    if (state.expiry.length === 5) {
       cardCvcRef.current.focus();
     }
-  }, [expiry]);
+  }, [state.expiry]);
 
   useEffect(() => {
-    if (cvc.length === 3) {
+    if (state.cvc.length === 3) {
       document.activeElement.blur();
     }
-  }, [cvc]);
+  }, [state.cvc]);
 
   // change state of card number input based on input value
   const handleNumberInput = (e) => {
-    setNumber(e.target.value);
+    //setNumber(e.target.value);
+    dispatch({ type: "number", data: e.target.value });
   };
 
   // if card number is incomplete when a user is changing focus of the input, set the numberErr state to true
   const handleNumberBlur = (e) => {
-    if (e.target.value.length < 19) {
-      setNumberErr(true);
-    } else {
-      setNumberErr(false);
-    }
+    dispatch({ type: "numberErr", data: e.target.value.length < 19 });
   };
 
   // change state of card expiry input based on input value
   const handleExpiryInput = (e) => {
-    setExpiry(e.target.value);
+    dispatch({ type: "expiry", data: e.target.value });
   };
 
   // if card expiry is incomplete when a user is changing focus of the input, set the expiryErr state to true
   const handleExpiryBlur = (e) => {
-    if (e.target.value.length < 5) {
-      setExpiryErr(true);
-    } else {
-      setExpiryErr(false);
-    }
+    dispatch({ type: "expiryErr", data: e.target.value.length < 5 });
   };
 
   // change state of cardholder name input based on input value (accepting only letters)
   const handleNameInput = (e) => {
-    setName(e.target.value.replace(/[^a-zA-Zæøå\s]*$/gi, ""));
+    dispatch({
+      type: "name",
+      data: e.target.value.replace(/[^a-zA-Zæøå\s]*$/gi, ""),
+    });
   };
 
   // if cardholder name contains less than 3 letters when a user is changing focus of the input, set the nameErr state to true
   const handleNameBlur = (e) => {
-    if (e.target.value.length < 3) {
-      setNameErr(true);
-    } else {
-      setNameErr(false);
-    }
+    dispatch({ type: "nameErr", data: e.target.value.length < 3 });
   };
 
   // change state of security code input based on input value
   const handleCvcInput = (e) => {
-    setCvc(e.target.value);
+    dispatch({ type: "cvc", data: e.target.value });
   };
 
   // if security code is incomplete when a user is changing focus of the input, set the cvcErr state to true
   const handleCvcBlur = (e) => {
-    if (e.target.value.length < 3) {
-      setCvcErr(true);
-    } else {
-      setCvcErr(false);
-    }
+    dispatch({ type: "cvcErr", data: e.target.value.length < 3 });
   };
 
   const focusOnError = (errorField) => {
@@ -112,13 +127,13 @@ function Creditcard() {
   };
 
   const checkForErrors = () => {
-    if (number.length < 19) {
+    if (state.number.length < 19) {
       return cardNumberRef;
-    } else if (name.length < 3) {
+    } else if (state.name.length < 3) {
       return cardNameRef;
-    } else if (expiry.length < 5) {
+    } else if (state.expiry.length < 5) {
       return cardExpiryRef;
-    } else if (cvc.length < 3) {
+    } else if (state.cvc.length < 3) {
       return cardCvcRef;
     }
   };
@@ -146,26 +161,55 @@ function Creditcard() {
         <form>
           {/* card number input field starts here*/}
           <div className="line line_one">
-            <label htmlFor="card-number" ref={cardNumberRef} className="label number">
+            <label
+              htmlFor="card-number"
+              ref={cardNumberRef}
+              className="label number"
+            >
               Card number
             </label>
             <div className="input_wrapper">
               <MaskedInput
-                mask={[/[1-9]/, /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, /\d/]}
+                mask={[
+                  /[1-9]/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                ]}
                 autoFocus
-                className={`form-control ${numberErr ? "incomplete" : ""}`}
+                className={`form-control ${
+                  state.numberErr ? "incomplete" : ""
+                }`}
                 placeholder="1234 5678 9012 3456"
                 guide={false}
-                value={number}
+                value={state.number}
                 id="card-number"
                 name="number"
                 inputMode="numeric"
                 onChange={handleNumberInput}
                 onBlur={handleNumberBlur}
               />
-              <SuccessMessage show={number.length === 19} />
+              <SuccessMessage show={state.number.length === 19} />
             </div>
-            <ErrorMessage text={"Credit card number must be 16 digits"} show={numberErr} />
+            <ErrorMessage
+              text={"Credit card number must be 16 digits"}
+              show={state.numberErr}
+            />
           </div>
           {/* card number input field ends here*/}
 
@@ -178,42 +222,54 @@ function Creditcard() {
               <input
                 type="text"
                 name="name"
-                className={`${nameErr ? "incomplete" : ""}`}
+                className={`${state.nameErr ? "incomplete" : ""}`}
                 id="card-name"
                 placeholder="John Doe"
                 ref={cardNameRef}
-                value={name}
+                value={state.name}
                 onChange={handleNameInput}
                 onBlur={handleNameBlur}
               />
-              <SuccessMessage show={name.length > 2} />
+              <SuccessMessage show={state.name.length > 2} />
             </div>
-            <ErrorMessage text={"Please enter your name"} show={nameErr} />
+            <ErrorMessage
+              text={"Please enter your name"}
+              show={state.nameErr}
+            />
           </div>
           {/* cardholder name input field ends here*/}
 
           <div className="line line_three">
             {/* card expiry input field starts here*/}
             <div className="column_one">
-              <label htmlFor="card-expiry" className="label expiry" ref={cardExpiryRef}>
+              <label
+                htmlFor="card-expiry"
+                className="label expiry"
+                ref={cardExpiryRef}
+              >
                 Expiry date
               </label>
               <div className="input_wrapper">
                 <MaskedInput
                   mask={[/[0-9]/, /\d/, "/", /\d/, /\d/]}
-                  className={`form-control ${expiryErr ? "incomplete" : ""}`}
+                  className={`form-control ${
+                    state.expiryErr ? "incomplete" : ""
+                  }`}
                   placeholder="04/23"
                   guide={false}
-                  value={expiry}
+                  value={state.expiry}
                   id="card-expiry"
                   name="name"
                   inputMode="numeric"
                   onChange={handleExpiryInput}
                   onBlur={handleExpiryBlur}
                 />
-                <SuccessMessage show={expiry.length === 5} />
+                <SuccessMessage show={state.expiry.length === 5} />
               </div>
-              <ErrorMessage text={"Expiry date must be 4 digits"} show={expiryErr} />
+              <ErrorMessage
+                text={"Expiry date must be 4 digits"}
+                show={state.expiryErr}
+              />
             </div>
             {/* card expiry input field ends here*/}
 
@@ -225,26 +281,34 @@ function Creditcard() {
               <div className="input_wrapper">
                 <MaskedInput
                   mask={[/[0-9]/, /\d/, /\d/, /\d/]}
-                  className={`form-control ${cvcErr ? "incomplete" : ""}`}
+                  className={`form-control ${state.cvcErr ? "incomplete" : ""}`}
                   placeholder="123"
                   guide={false}
-                  value={cvc}
+                  value={state.cvc}
                   id="card-cvc"
                   name="cvc"
                   inputMode="numeric"
                   onChange={handleCvcInput}
                   onBlur={handleCvcBlur}
                 />
-                <SuccessMessage show={cvc.length === 3} />
+                <SuccessMessage show={state.cvc.length === 3} />
               </div>
-              <ErrorMessage text={"Security code must be 3 digits"} show={cvcErr} />
+              <ErrorMessage
+                text={"Security code must be 3 digits"}
+                show={state.cvcErr}
+              />
             </div>
             {/* security code input field ends here*/}
           </div>
 
           {/* submit button starts here*/}
           <div className="line line_four">
-            <button className="submit_btn" type="submit" ref={submitButton} onClick={handleSubmit}>
+            <button
+              className="submit_btn"
+              type="submit"
+              ref={submitButton}
+              onClick={handleSubmit}
+            >
               order
             </button>
           </div>
